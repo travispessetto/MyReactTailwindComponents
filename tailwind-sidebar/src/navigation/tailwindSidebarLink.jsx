9a10940cf666087e6958bb9a0139bebea18712d4
Travis Pessetto <travis@pessetto.com>
2024-02-04 00:04:52 -0700

    Fix sidebar link

--- tailwind-sidebar/src/navigation/tailwindSidebarLink.jsx
+++ tailwind-sidebar/src/navigation/tailwindSidebarLink.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function TailwindSidebarLink({href,display, padding, children, target})
 {
+
+    // so purge css can recognize values
+    const paddingClasses = {
+        '4': 'pl-4',
+        '8': 'pl-8',
+        // Add more mappings as needed
+    };
+
+    const paddingLeftClass = paddingClasses[padding] || 'pl-4';
+
     return(
-        <a href={href} className={`flex flex-row w-full block ${padding ? `pl-${padding} pr-4` : 'pr-4 pl-4'} py-4 border-b items-center`} target={target} rel={target === '_blank' ? 'noopener noreferrer' : undefined}>
+        <a href={href} className={`flex flex-row w-full block pr-4 ${paddingLeftClass} py-4 border-b items-center`} target={target} rel={target === '_blank' ? 'noopener noreferrer' : undefined}>
             {display}
             {children}
         </a>
